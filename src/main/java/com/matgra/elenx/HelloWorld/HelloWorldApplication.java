69388a7bd5630d723a7c172601980304a7af2938
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-03-28 19:16:42 +0200

    Init commit

---
+++ src/main/java/com/matgra/elenx/HelloWorld/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.matgra.elenx.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
